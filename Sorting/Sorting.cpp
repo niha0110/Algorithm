1d2313affa9d3c8f916a7fe1a43ad08afa3e2f4c
niha0110 <56798162+niha0110@users.noreply.github.com>
2019-11-06 00:13:47 +0530

    few changes

--- Sorting/Sorting.cpp
+++ Sorting/Sorting.cpp
@@ -8,5 +8,11 @@ int main()
 	selectionSort s;
 	s.sort();
 	s.print();
+
+	int arr[5] = { 22,55,11,66,88 };
+	selectionSort s1(arr,(sizeof(arr)/sizeof(int)));
+	s1.print();
+	s1.sort();
+	s1.print();
 }
 
